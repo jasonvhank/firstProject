bfc669c3eead0e5281251d9f011fc7c3bd89a63a
jasonvhank <92424696+jasonvhank@users.noreply.github.com>
2021-10-20 18:40:47 +0800

    第１章　作業 2
    Button元件應用

--- App.js
+++ App.js
@@ -1,65 +1,86 @@
 import { setStatusBarNetworkActivityIndicatorVisible, StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 //版面樣式
 export default function App() {
   //初始化
-  const [phone, setPhone] = useState('')
+  const [imgA, setImgA] = useState(require('./src/img/manager.png'))
+  const [countA, setCountA] = useState(0)
+  const [countB, setCountB] = useState(0)
   //功能執行
-  const getPhone = () => {
-    if (phone === '0912345678') {
-      return <Text style={{ color: 'yellow', fontSize: 40 }}>輸入成功！</Text>
-    } else {
-      return <Text style={{ color: 'red', fontSize: 40 }}>手機輸入錯誤！</Text>
-    }
-  }
 
+
+  //圖片來源https://tw.clipart.me/17098/10-user-icons-free-psd-and-png
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>第１章　作業 １</Text>
-      <Text style={styles.mainText}>TextInput元件應用</Text>
-      <TextInput
-        style={{
-          height: 60, width: 350, borderRadius: 10,
-          backgroundColor: 'gray', color: 'white',
-          fontSize: 40, textAlign: 'left', marginTop: 100,
-        }}
-        onChangeText={(text) => setPhone(text)}
-        value={phone}
-        maxLength={10}
-        placeholder='請輸入手機號碼'
-        keyboardType={'numeric'}
-        underlineColorAndroid='yellow'
-      //secureTextEntry={true}
-      //editable={false}
-      //autoFocus={false}
+      <Text style={styles.titleText1}>第１章　作業 2</Text>
+      <Text style={styles.titleText2}>Button元件應用</Text>
+      <Image
+        style={{ width: 300, height: 400 }}
+        source={imgA}
       />
 
-      <Text style={{ color: 'yellow', fontSize: 35 }}>您輸入的手機號碼是：{phone}</Text>
-      {getPhone()}
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity
+          style={styles.buttonChange}
+          onPress={() => {
+            setCountA(countA + 1)
+            setImgA(require('./src/img/manager.png'))
+          }}>
+          <Text style={styles.buttonText}>使用者A</Text>
+        </TouchableOpacity>
 
+        <TouchableOpacity
+          style={styles.buttonChange}
+          onPress={() => {
+            setCountB(countB + 1)
+            setImgA(require('./src/img/support.png'))
+          }} >
+          <Text style={styles.buttonText}>使用者B</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={styles.mainText}>A使用次數：{countA}</Text>
+        <TouchableOpacity
+          style={styles.buttonClear}
+          onPress={() => setCountA(0)}>
+          <Text style={styles.buttonText}>清空A紀錄</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={styles.mainText}>B使用次數：{countB}</Text>
+        <TouchableOpacity
+          style={styles.buttonClear}
+          onPress={() => setCountB(0)}>
+          <Text style={styles.buttonText}>清空B紀錄</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 //樣式表
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'skyblue',
-    alignItems: 'center',
-    color: 'red'
-    //backgroundColor: 'skyblue',
-    //alignItems: 'center',
-    //justifyContent: 'center',
+    flex: 1, backgroundColor: 'skyblue', alignItems: 'center', color: 'red'
   },
-  titleText: {
-    marginTop: 60,
-    fontSize: 40,
-    color: 'red'
+  titleText1: {
+    marginTop: 60, fontSize: 40, color: 'red'
+  },
+  titleText2: {
+    fontSize: 40, color: 'red'
   },
   mainText: {
-    fontSize: 40,
-    color: 'red'
+    color: 'yellow', fontSize: 35
+  },
+  buttonText: {
+    color: 'white', textAlign: 'center', fontSize: 20
+  },
+  buttonChange: {
+    width: 100, height: 40, backgroundColor: '#00aeef', borderRadius: 20, justifyContent: 'center', margin: 20
+  },
+  buttonClear: {
+    width: 120, height: 40, backgroundColor: '#00aeef', borderRadius: 20, justifyContent: 'center', margin: 5
   }
 });
---
+++ src/test/App1-1.js
@@ -0,0 +1,65 @@
+import { setStatusBarNetworkActivityIndicatorVisible, StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+
+//版面樣式
+export default function App() {
+  //初始化
+  const [phone, setPhone] = useState('')
+  //功能執行
+  const getPhone = () => {
+    if (phone === '0912345678') {
+      return <Text style={{ color: 'yellow', fontSize: 40 }}>輸入成功！</Text>
+    } else {
+      return <Text style={{ color: 'red', fontSize: 40 }}>手機輸入錯誤！</Text>
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>第１章　作業 １</Text>
+      <Text style={styles.mainText}>TextInput元件應用</Text>
+      <TextInput
+        style={{
+          height: 60, width: 350, borderRadius: 10,
+          backgroundColor: 'gray', color: 'white',
+          fontSize: 40, textAlign: 'left', marginTop: 100,
+        }}
+        onChangeText={(text) => setPhone(text)}
+        value={phone}
+        maxLength={10}
+        placeholder='請輸入手機號碼'
+        keyboardType={'numeric'}
+        underlineColorAndroid='yellow'
+      //secureTextEntry={true}
+      //editable={false}
+      //autoFocus={false}
+      />
+
+      <Text style={{ color: 'yellow', fontSize: 35 }}>您輸入的手機號碼是：{phone}</Text>
+      {getPhone()}
+
+    </View>
+  );
+}
+//樣式表
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'skyblue',
+    alignItems: 'center',
+    color: 'red'
+    //backgroundColor: 'skyblue',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  titleText: {
+    marginTop: 60,
+    fontSize: 40,
+    color: 'red'
+  },
+  mainText: {
+    fontSize: 40,
+    color: 'red'
+  }
+});
